a340a4c97dc02cd3a562d122cab733d68bf391a5
Jessica Lohse <jalohse62442@gmail.com>
2015-06-18 11:13:12 -0400

    Adding Michaela's design!

---
+++ index.php
@@ -0,0 +1,227 @@
+<!doctype html>
+<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Jessica Lohse</title>
+
+    <link rel="stylesheet" type="text/css" href="css/foundation.min.css">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+<header>
+
+    <div class="row">
+
+        <div class="small-3 columns"><img src="img/logo-01.svg" class="logo" alt="jessica-logo"></div>
+
+        <nav class="small-9 small-offset-0  medium-6 medium-offset-3 large-4 large-offset-4 columns">
+            <ul>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Projects</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </nav>
+
+    </div>
+
+    <div class="row full-width">
+        <div class="large-screen-img">
+            <h2><span>Android. IOS. Web Backend.</span></h2>
+            <img src="img/jessicaHeader1.jpg">
+
+        </div>
+        <div class="small-screen-container"><h2><span>Android. IOS. Web Backend.</span></h2>
+            <img class="small-screen-img" src="img/jessicaHeader2.jpg">
+
+
+        </div>
+
+
+    </div>
+</header>
+
+<section id="about">
+    <h3>About</h3>
+
+    <div class="row">
+        <div class="small-12 medium-7 columns">
+
+
+            <p class="about-text">Tart wafer bonbon. Muffin carrot cake bonbon halvah marzipan fruitcake lollipop.
+                Caramels powder lemon
+                drops
+                lollipop macaroon gummi bears donut sweet roll powder. Sweet roll danish lollipop sweet roll apple pie.
+                Pudding
+                jujubes caramels sweet roll pudding cheesecake pie jujubes. Jelly beans soufflé wafer halvah caramels
+                lollipop
+                marzipan sugar plum. Sugar plum brownie topping lollipop tart macaroon candy canes halvah macaroon.
+                Chupa chups
+                soufflé tiramisu candy croissant tootsie roll. Pastry oat cake candy canes chupa chups jelly beans
+                halvah carrot
+                cake. Chocolate cake icing chocolate chupa chups toffee candy dragée gummi bears icing. Candy oat cake
+                cookie
+                candy canes donut gingerbread lemon drops cake wafer. Jujubes marzipan cheesecake tart toffee soufflé.
+
+                Chupa chups soufflé tiramisu candy croissant tootsie roll. Pastry oat cake candy canes chupa chups jelly
+                beans
+                halvah carrot cake. Chocolate cake icing chocolate chupa chups toffee candy dragée gummi bears icing.
+                Candy oat
+                cake cookie candy canes donut gingerbread lemon drops cake wafer. Jujubes marzipan cheesecake tart
+                toffee
+                soufflé.
+
+                Chupa chups soufflé tiramisu candy croissant tootsie roll. Pastry oat cake candy canes chupa chups jelly
+                beans
+                halvah carrot cake. Chocolate cake icing chocolate chupa chups toffee candy dragée gummi bears icing.
+                Candy oat
+                cake cookie candy canes donut gingerbread lemon drops cake wafer. Jujubes marzipan cheesecake tart
+                toffee
+                soufflé.</p>
+        </div>
+
+        <div class="small-0 medium-5 large-4 columns about-image">
+            <img src="img/JessicaAbout.jpg">
+        </div>
+    </div>
+</section>
+
+<section id="projects">
+    <h3>Projects</h3>
+
+    <div class="row">
+        <div class="individual-project small-12 large-4  large-offset-0 columns">
+            <div class="project-image small-10 small-offset-1 medium-4 medium-offset-4 large-10 large-offset-1 columns">
+                <img src="img/placeholderPhoneProject.png">
+            </div>
+            <div class="project-description small-12 columns">
+                <h4>Project Name</h4>
+
+                <p>Chupa chups soufflé tiramisu candy croissant tootsie roll. Pastry oat cake candy canes chupa chups
+                    jelly beans
+                    halvah carrot cake. Chocolate cake icing chocolate chupa chups toffee candy dragée gummi bears
+                    icing. Candy oat
+                    cake cookie candy canes donut gingerbread lemon drops cake wafer. Jujubes marzipan cheesecake tart
+                    toffee
+                    soufflé.</p>
+            </div>
+        </div>
+        <div class="individual-project small-12 large-4  large-offset-0 columns">
+            <div class="project-image small-10 small-offset-1 medium-4 medium-offset-4 large-10 large-offset-1 columns">
+                <img src="img/placeholderPhoneProject.png">
+            </div>
+            <div class="project-description small-12 columns">
+                <h4>Project Name</h4>
+
+                <p>Chupa chups soufflé tiramisu candy croissant tootsie roll. Pastry oat cake candy canes chupa chups
+                    jelly beans
+                    halvah carrot cake. Chocolate cake icing chocolate chupa chups toffee candy dragée gummi bears
+                    icing. Candy oat
+                    cake cookie candy canes donut gingerbread lemon drops cake wafer. Jujubes marzipan cheesecake tart
+                    toffee
+                    soufflé.</p>
+            </div>
+        </div>
+        <div class="individual-project large-4  large-offset-0 columns">
+            <div class="project-image small-10 small-offset-1 medium-4 medium-offset-4 large-10 large-offset-1 columns">
+                <img src="img/placeholderPhoneProject.png">
+            </div>
+            <div class="project-description small-12 columns">
+                <h4>Project Name</h4>
+
+                <p>Chupa chups soufflé tiramisu candy croissant tootsie roll. Pastry oat cake candy canes chupa chups
+                    jelly beans
+                    halvah carrot cake. Chocolate cake icing chocolate chupa chups toffee candy dragée gummi bears
+                    icing. Candy oat
+                    cake cookie candy canes donut gingerbread lemon drops cake wafer. Jujubes marzipan cheesecake tart
+                    toffee
+                    soufflé.</p>
+            </div>
+        </div>
+        <div class="individual-project large-4  large-offset-0">
+            <div class="project-image small-10 small-offset-1 medium-4 medium-offset-4 large-10 large-offset-1 columns">
+                <img src="img/placeholderPhoneProject.png">
+            </div>
+            <div class="project-description small-12 columns">
+                <h4>Project Name</h4>
+
+                <p>Chupa chups soufflé tiramisu candy croissant tootsie roll. Pastry oat cake candy canes chupa chups
+                    jelly beans
+                    halvah carrot cake. Chocolate cake icing chocolate chupa chups toffee candy dragée gummi bears
+                    icing. Candy oat
+                    cake cookie candy canes donut gingerbread lemon drops cake wafer. Jujubes marzipan cheesecake tart
+                    toffee
+                    soufflé.</p>
+            </div>
+        </div>
+        <div class="project-arrow small-1">
+            <img src="img/arrow-03-03.svg">
+        </div>
+    </div>
+</section>
+
+<section id="contact">
+    <h3>Contact</h3>
+
+    <form>
+        <div class="row">
+            <div class="large-12 columns">
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Email" />
+                <textarea placeholder="Message"></textarea>
+
+            </div>
+            <div class="small-2 small-offset-8 large-offset-10">
+            <button>Submit</button>
+            </div>
+        </div>
+
+    </form>
+
+    <div class="row">
+        <p class="small-12 columns">Chupa chups soufflé tiramisu candy croissant tootsie roll. Pastry oat cake candy
+            canes chupa chups
+            jelly beans
+            halvah carrot cake. Chocolate cake icing chocolate chupa chups toffee candy dragée gummi bears
+            icing. Candy oat
+            cake cookie candy canes donut gingerbread lemon drops cake wafer. Jujubes marzipan cheesecake tart
+            toffee
+            soufflé.</p>
+    </div>
+
+
+</section>
+
+<footer>
+    <div class="row">
+        <img src="img/jessicaHeader1.jpg" class="fullWidth small-12">
+    </div>
+    <div class="row">
+        <div class="social-media small-5 columns">
+            <ul>
+                <li><i class="fa fa-github fa-lg"></i></li>
+            </ul>
+        </div>
+
+        <div class="small-7 large-4 large-offset-3 columns">
+            <nav>
+                <ul>
+                    <li>About</li>
+                    <li>Projects</li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</footer>
+</body>
+
+<script type="text/javascript" src="js/foundation.min.js"></script>
+</html>
